2468c21168fee58203e9c9527b88803fc21ff8c6
jonaxd <jmn_3@hotmail.com>
2014-09-27 12:11:16 -0500

    5to

--- proy01a/src/refactoring/lab/logica/Fecha.java
+++ proy01a/src/refactoring/lab/logica/Fecha.java
@@ -4,7 +4,7 @@ public class Fecha {
 	private int dia;
 	private int mes;
 	private int anio;
-	private int anio4;
+	private int anio5;
 	public Fecha(int dia, int mes, int anio) {
 		this.dia = dia;
 		this.mes = mes;
